23ba1807b62af52b4fa86607a7f414128884ff95
Alfredo Miranda <alfredo@miranda.io>
2020-08-16 14:27:39 -0500

    Make headers slanted

--- src/public/target/ClassicAssociationsTable.js
+++ src/public/target/ClassicAssociationsTable.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/client';
-import { useTheme } from '@material-ui/core/styles';
+import { useTheme, makeStyles } from '@material-ui/core/styles';
 import { client3 } from './../client';
 import { lighten } from 'polished';
-import Table from '../common/Table/Table';
 
 const TARGET_ASSOCIATIONS_QUERY = gql`
   query TargetAssociationsQuery($ensemblId: String!, $page: Pagination!) {
@@ -92,11 +91,43 @@ const dataTypeOrder = [
   'animal_model',
 ];
 
+const useStyles = makeStyles(theme => ({
+  heatmap: {
+    borderCollapse: 'collapse',
+  },
+  rotate: {
+    height: '140px',
+    whiteSpace: 'nowrap',
+  },
+  headerDiv: {
+    transform: 'translate(17px, 50px) rotate(315deg)',
+    width: '30px',
+  },
+  headerSpan: {
+    borderBottom: '1px solid #ccc',
+    padding: '5px 10px',
+  },
+  dataCell: {
+    width: '30px',
+    border: '1px solid #ccc',
+    backgroundColor: 'papayawhip',
+  },
+  nameHeader: {
+    verticalAlign: 'bottom',
+  },
+  name: {
+    maxWidth: '250px',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+}));
+
 const ClassicAssociationsTable = ({ ensgId, dataTypes }) => {
-  console.log('dataTypes', dataTypes);
   const theme = useTheme();
   const [page, setPage] = useState(0);
-  const [pageSize, setPageSize] = useState(5);
+  const [pageSize, setPageSize] = useState(50);
+  const classes = useStyles();
 
   const { loading, error, data } = useQuery(TARGET_ASSOCIATIONS_QUERY, {
     variables: {
@@ -106,44 +137,75 @@ const ClassicAssociationsTable = ({ ensgId, dataTypes }) => {
     client: client3,
   });
 
-  const handlePageChange = page => {
-    setPage(page);
-  };
-
-  const handleRowsPerPageChange = pageSize => {
-    setPageSize(pageSize);
-  };
-
   if (error) return null;
-  // const rows = getRows(data?.target.associatedDiseases ?? [], dataTypes);
   const rows = data?.target.associatedDiseases ?? [];
 
-  console.log('rows', rows);
-
   return (
-    <table>
+    <table className={classes.heatmap}>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Overall Association Score</th>
-          <th>Genetic associations</th>
-          <th>Somatic mutations</th>
-          <th>Drugs</th>
-          <th>Pathways & systems biology</th>
-          <th>RNA expression</th>
-          <th>Text mining</th>
-          <th>Animal models</th>
+          <th className={classes.nameHeader}>
+            <div>
+              <span>Name</span>
+            </div>
+          </th>
+          <th className={classes.rotate}>
+            <div className={classes.headerDiv}>
+              <span className={classes.headerSpan}>
+                Overall Association Score
+              </span>
+            </div>
+          </th>
+          <th className={classes.rotate}>
+            <div className={classes.headerDiv}>
+              <span className={classes.headerSpan}>Genetic associations</span>
+            </div>
+          </th>
+          <th className={classes.rotate}>
+            <div className={classes.headerDiv}>
+              <span className={classes.headerSpan}>Somatic mutations</span>
+            </div>
+          </th>
+          <th className={classes.rotate}>
+            <div className={classes.headerDiv}>
+              <span className={classes.headerSpan}>Drugs</span>
+            </div>
+          </th>
+          <th className={classes.rotate}>
+            <div className={classes.headerDiv}>
+              <span className={classes.headerSpan}>
+                Pathways & systems biology
+              </span>
+            </div>
+          </th>
+          <th className={classes.rotate}>
+            <div className={classes.headerDiv}>
+              <span className={classes.headerSpan}>RNA expression</span>
+            </div>
+          </th>
+          <th className={classes.rotate}>
+            <div className={classes.headerDiv}>
+              <span className={classes.headerSpan}>Text mining</span>
+            </div>
+          </th>
+          <th className={classes.rotate}>
+            <div className={classes.headerDiv}>
+              <span className={classes.headerSpan}>Animal models</span>
+            </div>
+          </th>
         </tr>
       </thead>
       <tbody>
         {rows.map(row => {
           return (
             <tr key={row.disease.id}>
-              <td>{row.disease.name}</td>
-              <td>{row.score}</td>
+              <td>
+                <div className={classes.name}>{row.disease.name}</div>
+              </td>
+              <td className={classes.dataCell} />
               {dataTypeOrder.map(dataType => {
                 const index = row.idPerDT.indexOf(dataType);
-                return <td key={dataType}>{row.scorePerDT[index]}</td>;
+                return <td key={dataType} className={classes.dataCell} />;
               })}
             </tr>
           );
